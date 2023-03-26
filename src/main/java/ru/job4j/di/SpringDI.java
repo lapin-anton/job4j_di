746684a6538a8d67c944ee5d5a202704092bdebc
anton-lapin <lapin_antoine@mail.ru>
2023-03-26 10:59:46 +0300

    1. Spring DI. Запуск [#2091]

---
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -0,0 +1,21 @@
+package ru.job4j.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.job4j.di.input.ConsoleInput;
+import ru.job4j.di.store.Store;
+
+public class SpringDI {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
+        ui.init();
+    }
+}
